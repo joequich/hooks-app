db6c288c10287835be10e5adc99d083bdc2b6d4c
Joseph Quispe <josepheqch@gmail.com>
2021-07-26 21:23:29 -0500

    useReducer TodoAPP - delete and complete action

--- src/components/08-useReducer/TodoApp.tsx
+++ src/components/08-useReducer/TodoApp.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useForm } from '../../hooks/useForm';
 
 import './styles.css';
-import { todoReducer, IStateTodo } from './todoReducer';
+import { todoReducer, IStateTodo, IActionTodo } from './todoReducer';
 
 interface FormValues {
     description: string;
@@ -39,13 +39,29 @@ export const TodoApp = () => {
             done: false
         };
 
-        const action = {
+        const action: IActionTodo = {
             type: 'add',
             payload: newTodo
         };
 
         dispatch(action);
         reset();
+    }
+
+    const handleDelete = (todoId: number) => {
+        const action: IActionTodo = {
+            type: 'delete',
+            payload: todoId
+        }
+
+        dispatch(action);
+    }
+
+    const handleToggle = (todoId: number) => {
+        dispatch({
+            type: 'toggle',
+            payload: todoId
+        });
     }
 
     return (
@@ -62,8 +78,8 @@ export const TodoApp = () => {
                                     key={todo.id}
                                     className="list-group-item"
                                 >
-                                    <p className="text-center">{idx+1}. {todo.desc}</p>
-                                    <button className="btn btn-danger">Delete</button>
+                                    <p className={ `${ todo.done && 'complete'}` } onClick={() => handleToggle(todo.id)}>{idx+1}. {todo.desc}</p>
+                                    <button className="btn btn-danger" onClick={() => handleDelete(todo.id)}>Delete</button>
                                 </li>
                             ))
                         }
--- src/components/08-useReducer/todoReducer.ts
+++ src/components/08-useReducer/todoReducer.ts
@@ -4,20 +4,37 @@ export interface IStateTodo {
     done: boolean
 }
 
-// export type IActionTodo = 
-//     | { type: "add"; payload: IStateTodo }
-//     | { type: "modify"; payload: IStateTodo };
-
-export interface IActionTodo {
-    type: string,
-    payload: IStateTodo
-}
+export type IActionTodo = 
+    | { type: 'add', payload: IStateTodo }
+    | { type: 'delete' | 'toggle', payload: number };
 
 export const todoReducer = (state: IStateTodo[], action: IActionTodo) => {
     switch (action.type) {
         case 'add':
             return [...state, action.payload];
     
+        case 'delete':
+            return state.filter(todo => todo.id !== action.payload);
+
+        case 'toggle':
+            return state.map(todo => 
+                (todo.id === action.payload)
+                    ? { ...todo, done: !todo.done }
+                    : todo
+            );
+
+        // case 'toggle-old':
+        //     return state.map(todo => {
+        //         if (todo.id === action.payload) {
+        //             return {
+        //                 ...todo,
+        //                 done: !todo.done
+        //             };
+        //         } else {
+        //             return todo;
+        //         }
+        //     });
+
         default:
             return state;
     }
